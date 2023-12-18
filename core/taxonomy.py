b82b0eb237f6823267ba4a2bffb3a2adb7e69313
william-zhang-ml <william.zhang.ml@gmail.com>
2023-12-18 12:59:20 -0500

    add taxonomy utils, task heads, and task losses

---
+++ core/taxonomy.py
@@ -0,0 +1,250 @@
+"""
+Multi-taxonomy task heads, loss functions, and metrics.
+
+Consider a set of labels grouped into a taxonomy of coarser labels.
+There can be different but still-valid taxonomies.
+
+For example, consider the following labels and taxonomies.
+Labels -> apple, orange, beet, carrot
+Color -> {apple, beet}, {orange, carrot}
+Fruit -> {apple, orange}, {beet, carrot}
+"""
+from typing import Callable, Dict, List, Tuple
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+
+def invert_ontology(ontology: Dict[int, List[int]]) -> Dict[int, int]:
+    """Invert a "ontology group to members" map.
+
+    Args:
+        ontology (Dict[int, List[int]]): ontology group to member labels map
+
+    Returns:
+        Dict[int, int]: labels to ontology groupmap
+    """
+    inverse = {}
+    for group, group_labels in ontology.items():
+        for label in group_labels:
+            inverse[label] = group
+    return inverse
+
+
+ONTOLOGY_A = {
+    0: [0],
+    1: [1, 9],
+    2: [2, 3, 4, 5, 6, 7, 8],
+}
+LABEL_TO_GROUP_A = invert_ontology(ONTOLOGY_A)
+
+
+def apply_ontology(
+    labels: torch.Tensor,
+    ontology: Dict[int, List[int]]
+) -> torch.Tensor:
+    """Map labels to coarser labels according to an ontology.
+
+    Args:
+        labels (torch.Tensor): labels to map
+        ontology (Dict[int, List[int]]): ontology group to member labels map
+
+    Returns:
+        torch.Tensor: mapped labels
+    """
+    inverse = invert_ontology(ontology)
+    return torch.tensor([inverse[int(curr)] for curr in labels])
+
+
+def gather_outputs(
+    network: torch.nn.Module,
+    dataset: torch.utils.data.Dataset,
+    batch_size: int = 10,
+    device: str = 'cuda:0'
+) -> Tuple[List[torch.FloatTensor], torch.LongTensor]:
+    """Run entire dataset through a network (no grad).
+
+    Args:
+        network (torch.nn.Module): _description_
+        dataset (torch.utils.data.Dataset): _description_
+        batch_size (int, optional): _description_. Defaults to 10.
+
+    Returns:
+        Tuple[torch.FloatTensor, torch.LongTensor]: network outputs and labels
+    """
+    loader = DataLoader(dataset, batch_size, shuffle=False)
+    with torch.no_grad():
+        all_outputs = []
+        all_labels = []
+        for images, labels in loader:
+            outputs: List[torch.Tensor] = network(images.to(device))
+            all_outputs.append(outputs.cpu())
+            all_labels.append(labels)
+    all_outputs = [torch.cat(curr) for curr in zip(*all_outputs)]
+    all_labels = torch.cat(all_labels)
+    return all_outputs, all_labels
+
+
+class OntologyHead(nn.Module):
+    """Task head for labels and multiple label ontologies. """
+    def __init__(
+        self,
+        in_features: int,
+        num_classes: int,
+        ontologies: List[Dict[int, List[int]]]
+    ) -> None:
+        super().__init__()
+        if isinstance(ontologies, dict):
+            ontologies = [ontologies]
+
+        self.linear = nn.Linear(in_features, num_classes, bias=True)
+        self.ontologies = ontologies  # TODO - make deepcopy
+        self.adders = []
+        for ontology in ontologies:
+            # convert ontology to a matrix that implements score-adding
+            adder = torch.zeros(num_classes, len(ontology))
+            for parent, children in ontology.items():
+                adder[children, parent] = 1
+            self.adders.append(adder)
+
+        # sanity check
+        for outp in self(torch.rand(10, in_features)):
+            assert torch.allclose(outp.sum(dim=1), torch.tensor(1.))
+
+    def forward(self, features: torch.Tensor) -> List[torch.Tensor]:
+        """Compute class confidence scores and ontology confidence scores.
+
+        Args:
+            features (torch.Tensor): feature vectors from some backbone (B, C)
+
+        Returns:
+            List[torch.Tensor]: class and ontology confidence scores
+        """
+        scores = self.linear(features).softmax(dim=1)
+        outputs = [scores]
+        for adder in self.adders:
+            outputs.append(scores @ adder.to(scores.device))
+        return outputs
+
+    def compute_loss(
+        self,
+        outputs: List[torch.Tensor],
+        labels: torch.Tensor,
+        focal_gamma: float = None,
+        label_smoothing: float = None
+    ) -> List[torch.Tensor]:
+        """_summary_
+
+        Args:
+            outputs (List[torch.Tensor]): _description_
+            labels (torch.Tensor): _description_
+            focal_gamma (float, optional): _description_. Defaults to None.
+            label_smoothing (float, optional): _description_. Defaults to None.
+
+        Returns:
+            List[torch.Tensor]: _description_
+        """
+        loss_func = ClassBalancedLoss(
+            FocalEntropyWithSmoothing(focal_gamma, label_smoothing)
+        )
+        losses = []
+        losses.append(loss_func(outputs[0], labels))
+        for curr_output, ontology in zip(outputs[1:], self.ontologies):
+            losses.append(
+                loss_func(
+                    curr_output,
+                    apply_ontology(labels, ontology)
+                )
+            )
+        return losses
+
+    def compute_metrics(
+        self,
+        outputs: List[torch.Tensor],
+        labels: torch.Tensor,
+    ) -> Dict[str, float]:
+        """Foo. """
+        acc: List[float] = []
+        pred = outputs[0].argmax(dim=1)
+        acc.append(100 * (pred == labels).float().mean().item())
+        for outp, ontology in zip(outputs[1:], self.ontologies):
+            pred = outp.argmax(dim=1)
+            onto_labels = apply_ontology(labels, ontology)
+            acc.append(100 * (pred == onto_labels).float().mean().item())
+        return {
+            f'acc/{ii}': float(acc)
+            for ii, acc in enumerate(acc)
+        }
+
+
+class FocalEntropyWithSmoothing:
+    """Cross-entropy with focal loss envelopes and label smoothing. """
+    def __init__(
+        self,
+        focal_gamma: float = None,
+        label_smoothing: float = None
+    ) -> None:
+        self.focal = focal_gamma
+        self.smooth = label_smoothing
+
+    def __repr__(self) -> str:
+        return f'FocalEntropyWithSmoothing({self.focal}, {self.smooth})'
+
+    def __call__(
+        self,
+        soft: torch.Tensor,
+        labels: torch.Tensor
+    ) -> torch.Tensor:
+        """Compute cross-entropy.
+
+        Args:
+            soft (torch.Tensor): class confidence score.
+            labels (torch.Tensor): true class labels
+
+        Returns:
+            torch.Tensor: cross-entropy
+        """
+        assert soft.ndim == 2
+        assert labels.ndim == 1
+
+        # apply focal envelope
+        logsoft = soft.log()
+        if self.focal is not None:
+            logsoft = (1 - soft) ** self.focal * logsoft
+
+        # apply label smoothing
+        onehot = torch.eye(soft.shape[1], device=soft.device)[labels]
+        if self.smooth is not None:
+            onehot = (1 - self.smooth) * onehot
+            onehot = onehot + self.smooth / soft.shape[1]
+
+        return -(onehot * logsoft).sum(dim=1).mean()
+
+
+class ClassBalancedLoss:
+    """Utility class that averages loss by class not by sample. """
+    def __init__(self, dependency: Callable) -> None:
+        self.loss_func = dependency
+
+    def __repr__(self) -> str:
+        return f'ClassBalancedLoss({self.loss_func}))'
+
+    def __call__(
+        self,
+        output: torch.Tensor,
+        labels: torch.Tensor
+    ) -> torch.Tensor:
+        """Compute loss averaged by class.
+
+        Args:
+            output (torch.Tensor): network output
+            labels (torch.Tensor): class labels
+
+        Returns:
+            torch.Tensor: loss
+        """
+        per_class_loss = []
+        for curr_label in labels.unique():
+            mask = labels == curr_label
+            per_class_loss.append(self.loss_func(output[mask], labels[mask]))
+        return torch.stack(per_class_loss).mean()
