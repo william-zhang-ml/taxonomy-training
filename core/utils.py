a9d5ccf95bc0a8dee0d6e3df6a3fd9591836475e
william-zhang-ml <william.zhang.ml@gmail.com>
2023-12-18 09:45:37 -0500

    add class to name training runs and manage output writing

---
+++ core/utils.py
@@ -0,0 +1,67 @@
+"""Utility functions and classes. """
+import os
+from pathlib import Path
+import torch
+
+
+class Output:
+    """Training run output directory interface."""
+    def __init__(self, root, tag, exists: bool = False):
+        self.root = Path(root)
+        self.tag = tag
+        os.makedirs(self.checkpoint_dir, exist_ok=exists)
+
+    @property
+    def output_dir(self) -> Path:
+        """
+        Returns:
+            Path: path to outputs
+        """
+        return self.root / self.tag
+
+    @property
+    def checkpoint_dir(self) -> Path:
+        """
+        Returns:
+            Path: path to training checkpoints
+        """
+        return self.output_dir / 'checkpoints'
+
+    @property
+    def config_path(self) -> Path:
+        """
+        Returns:
+            Path: path to config file used to initiate training
+        """
+        return self.output_dir / 'config.yaml'
+
+    @property
+    def onnx_path(self) -> Path:
+        """
+        Returns:
+            Path: path to final weights
+        """
+        return self.output_dir / 'final.onnx'
+
+    def get_latest_checkpoint(self) -> dict:
+        """Load most-recent (alphabetical) training checkpoint's state.
+
+        Returns:
+            dict: checkpoint state
+        """
+        checkpoints = sorted(self.checkpoint_dir.glob('*.pt'))
+        return torch.load(checkpoints[-1])
+
+    def write_done_token(self) -> None:
+        """Write 'done.txt' to output directory to show training run done. """
+        token_path = self.output_dir / 'done.txt'
+        with open(token_path, 'w', encoding='utf-8') as file:
+            file.write('done')
+
+    def is_done(self) -> bool:
+        """Check whether a training run is done (via token).
+
+        Returns:
+            bool: whether 'done.txt' exists in the output directory
+        """
+        return os.path.isfile(self.output_dir / 'done.txt')
